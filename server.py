8fb54f3e15596cf1d6117b11d9bf0ceda09b9959
bashton <14bashton@gmail.com>
2022-11-11 16:07:21 -0700

    testing server out

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import stableDiffusion2
+# import stableDiffusion2
 import urllib.parse
 
 
 hostName = "0.0.0.0"
 # hostName = "localhost"
 serverPort = 6969
-imggen = stableDiffusion2.theAlgo()
+# imggen = stableDiffusion2.theAlgo()
 
 class MyServer(BaseHTTPRequestHandler):
+    protocol_version: str = 'HTTP/1.1'
     def do_GET(self):
         stuff = self.path.split("/")[2]
         decodedURL = urllib.parse.unquote(stuff)
-        stuff = imggen.generate(decodedURL)
+        response = b'{data: ' + bytes(decodedURL, 'utf-8') + b'}'
+        # stuff = imggen.generate(decodedURL)
         print(stuff)
-        self.send_response(200,stuff)
-        self.send_header("Content-type", "text/html")
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Content-Length", len(response))
         self.end_headers()
+        self.wfile.write(response)
         
 
 if __name__ == "__main__":        
