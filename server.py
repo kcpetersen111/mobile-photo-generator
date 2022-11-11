e11d7c1d1a7b7a8947dc8c016ac6ad9c8fab0cd2
Kaleb Petersen <kcpetersen111@gmail.com>
2022-11-11 15:06:45 -0700

    adding error handling

--- server.py
+++ server.py
@@ -11,15 +11,19 @@ imggen = stableDiffusion2.theAlgo()
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        givenPath = self.path.split("/")[2]
-        decodedURL = urllib.parse.unquote(givenPath)
-        imgLocation= imggen.generate(decodedURL)
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        self.send_response( message={"filePath":imgLocation})
-        print(imgLocation, '\n\n')
+        try:
+
+            givenPath = self.path.split("/")[2]
+            decodedURL = urllib.parse.unquote(givenPath)
+            imgLocation= imggen.generate(decodedURL)
+            # self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.send_response(code=200, message={"filePath":imgLocation})
+            print(imgLocation, '\n\n')
+        except:
+            self.send_error(500,"there was an error")
 
 
 if __name__ == "__main__":
