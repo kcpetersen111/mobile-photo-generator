2f6bfa24dc5ebb490393b027ed104c4e705c2e7c
Brendan Davidson <binary141@gmail.com>
2022-11-11 17:36:05 +0000

    Forced it to only generate a single image and return the image path when it is complete

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import stableDiffusion2
 
 def main():
     image = stableDiffusion2.theAlgo(512,640)# 384, 384)
-    image.generate("hello")
-
-    image.generate("hello1")
-
-    image.generate("hello2")
+    print(image.generate("Benjamin Ashton the Mac lover"))
 
 
 if __name__ == '__main__':
--- stableDiffusion2.py
+++ stableDiffusion2.py
@@ -9,17 +9,21 @@ class theAlgo:
         keras.mixed_precision.set_global_policy("mixed_float16")
         self.model = keras_cv.models.StableDiffusion(img_width, img_height)
 
-    def generate(self, prompt, batch_size=1):
+    def generate(self, prompt):
         if prompt is None:
             return 0
+
         # In case we want to count de time
         start_time = time.time()
 
-        images = self.model.text_to_image(prompt, batch_size)
+        images = self.model.text_to_image(prompt, batch_size=1)
 
-        for i in range(len(images)):
-            Image.fromarray(images[i]).save("%s%s.png" % (prompt, i+1))
+        # for i in range(len(images)):
+            # Image.fromarray(images[i]).save("%s%s.png" % (prompt, i+1))
+        img_location = "%s-%s.png" % (prompt, 1)
+        Image.fromarray(images[0]).save(img_location)
+
         total_time = time.time() - start_time
         print("total_time: ", total_time)
 
-        return 1
+        return img_location
