a7e7b4a0590f3035e0469aebc185d8e055e5c12b
Kaleb Petersen <kcpetersen111@gmail.com>
2022-11-11 15:29:50 -0700

    trying something new

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import stableDiffusion2
 import urllib.parse
+import json
 
 
 hostName = "0.0.0.0"
@@ -10,6 +11,7 @@ serverPort = 6969
 imggen = stableDiffusion2.theAlgo()
 
 class MyServer(BaseHTTPRequestHandler):
+    protocol_version: str = 'HTTP/1.1'
     def do_GET(self):
         try:
 
@@ -17,10 +19,13 @@ class MyServer(BaseHTTPRequestHandler):
             decodedURL = urllib.parse.unquote(givenPath)
             imgLocation= imggen.generate(decodedURL)
             # self.send_response(200)
+            
             self.send_header("Content-type", "application/json")
+            self.send_header("Content-Length", len(imgLocation))
             self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_response(code=200)
             self.end_headers()
-            self.send_response(code=200, message={"filePath":imgLocation})
+            self.wfile.write(json.dump( {"filePath":imgLocation}))
             print(imgLocation, '\n\n')
         except:
             self.send_error(500,"there was an error")
@@ -28,6 +33,7 @@ class MyServer(BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
+    
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
