1c6e0edb25a99b5101511f3e7269a7c7eace3d6f
Kaleb Petersen <kcpetersen111@gmail.com>
2022-11-11 11:46:56 -0700

    more work on the server, updated the address and now get the name as the response

--- server.py
+++ server.py
@@ -3,19 +3,20 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import stableDiffusion2
 
 
-hostName = "code.binary141.com"
+hostName = "0.0.0.0"
 # hostName = "localhost"
 serverPort = 6969
 imggen = stableDiffusion2.theAlgo()
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
         stuff = self.path.split("/")[1]
         imggen.generate(stuff)
         print(stuff)
+        self.send_response(200,stuff)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
