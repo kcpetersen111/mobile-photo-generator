1ae298bb1ccf6baf5b714d18cc87eb69bff90578
Brendan Davidson <binary141@gmail.com>
2022-11-12 05:54:12 +0000

    Reworked server file to provide a list of images, better error handling, and option to turn off loading stable diffusion for faster web server testing

--- server.py
+++ server.py
@@ -1,43 +1,74 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import stableDiffusion2
 import urllib.parse
 import time
-
+import os
+import json
 
 hostName = "0.0.0.0"
 # hostName = "localhost"
 serverPort = 6969
-imggen = stableDiffusion2.theAlgo()
+
+STABLE_DIFFUSION = True
+
+if STABLE_DIFFUSION:
+    import stableDiffusion2
+    imggen = stableDiffusion2.theAlgo()
 
 
 class MyServer(BaseHTTPRequestHandler):
     protocol_version: str = 'HTTP/1.1'
     def do_GET(self):
-        prompt = self.path.split("/")[2]
-        decodedURL = urllib.parse.unquote(prompt)
+        try:
+            if "ListImages" in self.path.split("/")[1]:
+                print("They are here!")
+                files = os.listdir("/opt/stableDiffusion")
+                arr = []
+                for file in files:
+                    if file.endswith(".png"):
+                        arr.append(file)
+                # print("files: ", files)
 
-        path = imggen.generate(decodedURL)
-        print(path)
+                response = b'{"images": ' + bytes('"' + ','.join(arr) + '"', 'utf-8') + b'}'
+                self.send_response(200)
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.send_header("Content-Length", len(response))
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
 
-        response = b'{"data": ' + bytes('"'+path+'"', 'utf-8') + b'}'
-        # print(webServer.client_address())
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header("Content-Length", len(response))
-        self.send_header("Content-type", "application/json")
-        # self.send_header("Connection", "close")
-        # self.send_header('Host', 'code.binary141.com')
-        # self.send_header('Origin', 'code.binary141.com')
-        # self.send_header("keep-alive", "timeout=1, max=10")
-        self.end_headers()
-        self.wfile.write(response)
-        self.close_connection = True
-        print(self.client_address)
 
+
+                self.wfile.write(response)
+                self.close_connection = True
+            else:
+                prompt = self.path.split("/")[2]
+                decodedURL = urllib.parse.unquote(prompt)
+
+                if STABLE_DIFFUSION:
+                    path = imggen.generate(decodedURL)
+                    print(path)
+
+                    response = b'{"data": ' + bytes('"'+path+'"', 'utf-8') + b'}'
+                else:
+                    response = "Hello"
+
+                # print(webServer.client_address())
+                self.send_response(200)
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.send_header("Content-Length", len(response))
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+
+                self.wfile.write(response)
+                self.close_connection = True
+                print(self.client_address)
+        except Exception as e:
+            print("The error was: \n", e)
+            self.send_error(500,"there was an error")
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
+    path = imggen.generate("warmup")
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
