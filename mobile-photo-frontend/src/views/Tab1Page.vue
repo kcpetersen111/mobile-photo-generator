98522f0e127ff4e0a3f51211a02c776d8269002f
Kaleb Petersen <kcpetersen111@gmail.com>
2022-11-12 07:53:09 -0700

    last minute

--- mobile-photo-frontend/src/views/Tab1Page.vue
+++ mobile-photo-frontend/src/views/Tab1Page.vue
@@ -2,18 +2,23 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <img v-bind:src="imageUrl">
-      <ion-item>
+      <ion-item v-if="!loading">
         <ion-label>Input:</ion-label>
         <ion-input type="text" placeholder="type something here..." v-model="input" v-on:keydown.enter="sendInput()"></ion-input>
         <ion-button shape="round" @click="sendInput()">GO!</ion-button>
       </ion-item>
+
+      <div v-else id="loadingAnimation" style="display:flex; justify-content:center; padding-top: 30px;">
+        {{this.loadingText}}
+    </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonInput, IonPage, IonContent } from '@ionic/vue';
+import { IonInput, IonPage, IonContent, createAnimation } from '@ionic/vue';
+import { ref } from 'vue';
 // import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default defineComponent({
@@ -22,21 +27,36 @@ export default defineComponent({
   methods: {
     sendInput: async function() {
       // alert(this.imageUrl)
+      this.loading = true;
       let response = await fetch("http://code.binary141.com:6969/" + this.size + "/" + encodeURIComponent(this.input), {
         method: 'GET',
         headers: {},
       })
       let data = await response.json()
-          this.imageUrl = data["data"];
-       
+      this.imageUrl = data["data"];
+      this.loading = false; 
     }
   },
   data(){
     return{
       input: '',
       imageUrl: '',
-      size: 1
+      loading:false,
+      size: 1,
+      loadingText: "Loading"
+      // elementRef: ref(),
     }
-  }
+  },
+  mounted(){
+    
+    setInterval(async () => { 
+       if(this.loadingText.length>9){
+          this.loadingText = "Loading"
+       } else {
+          this.loadingText += "."
+       }
+      }, 500);
+
+  },
 });
 </script>
