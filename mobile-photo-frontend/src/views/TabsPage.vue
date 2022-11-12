5328fd274b407fbc95f63d497df17699ad9aaf08
bashton <14bashton@gmail.com>
2022-11-12 02:43:46 -0700

    added tabs back for gallery

--- mobile-photo-frontend/src/views/TabsPage.vue
+++ mobile-photo-frontend/src/views/TabsPage.vue
@@ -2,22 +2,22 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <!-- <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="Generator" href="/generator">
           <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-label>Generator</ion-label>
         </ion-tab-button>
           
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <ion-tab-button tab="Gallery" href="/gallery">
           <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-label>Gallery</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
+        <!-- <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon :icon="square" />
           <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar> -->
+        </ion-tab-button> -->
+      </ion-tab-bar>
     </ion-tabs>
   </ion-page>
 </template>
@@ -29,7 +29,7 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
-  components: { IonTabs, IonPage, IonRouterOutlet },
+  components: { IonTabs, IonPage, IonRouterOutlet, IonTabButton, IonTabBar },
   setup() {
     return {
       ellipse, 
